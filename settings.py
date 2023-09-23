9e50be05f41d0921e9ead3ea36ccbc4e456ddf65
Yurii <yuriydorosh2005@gmail.com>
2023-09-23 18:30:37 +0300

    REFACTOR: black

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-urls: list[str] = [ 
+urls: list[str] = [
     "https://www.work.ua/jobs-python/",
     "https://www.work.ua/jobs-python+trainee/",
     "https://www.work.ua/jobs-python+junior/",
@@ -6,7 +6,9 @@ urls: list[str] = [
     "https://www.work.ua/jobs-django/",
 ]
 
-path_to_directory: str = "/home/apacer/Desktop/Work.ua-parser" # Path to directory where you want to save file with vacancies.
-csv_file_name: str = "Work-ua-parser.csv" # Name of your file(word ending: .csv)
+path_to_directory: str = "/home/apacer/Desktop/Work.ua-parser"  # Path to directory where you want to save file with vacancies.
+csv_file_name: str = "Work-ua-parser.csv"  # Name of your file(word ending: .csv)
 
-path_to_csv_file: str = path_to_directory + "/" + csv_file_name # Total path to the file
+path_to_csv_file: str = (
+    path_to_directory + "/" + csv_file_name
+)  # Total path to the file
